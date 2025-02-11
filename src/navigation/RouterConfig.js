06820629287f8ffb58e2b1520ccb5d5e68f1964c
Gihan Wijesinghe <gihantharaka76@gmail.com>
2025-02-12 02:13:46 +0530

    routes added and date fetching done for men & women clothing pages

--- src/navigation/RouterConfig.js
+++ src/navigation/RouterConfig.js
@@ -1,13 +1,16 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import { LandingPage } from '../pages/LandingPage'
-import { ROOT } from './Routes'
+import { MENS_CLOTHING_ROOT, ROOT, WOMENS_CLOTHING_ROOT } from './Routes'
+import { ProductList } from '../pages/ProductList'
 
 export const RouterConfig = () => {
     return (
         <Routes>
             {/* page routes */}
             <Route path={ROOT} element={<LandingPage />} />
+            <Route path={MENS_CLOTHING_ROOT} element={<ProductList />} />
+            <Route path={WOMENS_CLOTHING_ROOT} element={<ProductList />} />
         </Routes>
     )
 }
---
+++ src/pages/ProductList.js
@@ -0,0 +1,64 @@
+import React, { useEffect, useState } from 'react'
+import { useLocation } from 'react-router-dom'
+import { CATEGORY_END_POINT, MENS_CLOTHING, WOMENS_CLOTHING } from '../config/Constants';
+import { Grid2, Typography } from '@mui/material';
+import { ProductCard } from '../components/ProductCard';
+import { fetchProducts } from '../services/ProductServices';
+import { MENS_CLOTHING_ROOT, WOMENS_CLOTHING_ROOT } from '../navigation/Routes';
+
+const styles = {
+    category: {
+        fontWeight: 'bold',
+        marginBottom: '25px',
+        marginTop: '25px',
+        marginLeft: '100px'
+    },
+    grid: {
+        margin: "0 0 20px 0",
+        justifyContent: "center"
+    }
+};
+
+export const ProductList = () => {
+    const { pathname } = useLocation();
+    const [products, setProducts] = useState([]);
+
+    // fetch products data
+    useEffect(() => {
+        (async () => {
+            const response = await fetchProducts(pathname === MENS_CLOTHING_ROOT ? CATEGORY_END_POINT(MENS_CLOTHING?.toLowerCase()) : pathname === WOMENS_CLOTHING_ROOT ? CATEGORY_END_POINT(WOMENS_CLOTHING?.toLowerCase()) : '', '?sort=desc');
+            const productData = response?.map((d) => {
+                return {
+                    id: d?.id,
+                    title: d?.title,
+                    image: d?.image,
+                    price: d?.price,
+                    description: d?.description?.substring(0, 90),
+                    category: d?.category,
+                }
+            })
+            setProducts(productData);
+        })();
+    }, []);
+
+    return (
+        <>
+            <Typography variant='h6' sx={styles.category}>
+                {MENS_CLOTHING}
+            </Typography>
+            <Grid2 container spacing={3} sx={styles.grid}>
+                {products?.map((p, i) => (
+                    <Grid2 key={i} xs={12} sm={6} md={3}>
+                        <ProductCard
+                            title={p.title}
+                            imageURL={p.image}
+                            price={p.price}
+                            description={p.description}
+                            category={p.category}
+                        />
+                    </Grid2>
+                ))}
+            </Grid2>
+        </>
+    )
+}
